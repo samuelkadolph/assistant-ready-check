9dc0f15646576eb66da965b6e7c7b41025a0ae33
Samuel Kadolph <samuel@kadolph.com>
2010-12-22 19:12:34 -0500

    Remove whitespace from empty lines

--- AssistantReadyCheck.lua
+++ AssistantReadyCheck.lua
@@ -6,9 +6,8 @@ AssistantReadyCheck:RegisterEvent("READY_CHECK_FINISHED");
 local readyCheckPlayers, readyCheckAllReady;
 
 AssistantReadyCheck:SetScript("OnEvent", function(self, event, ...)
-	if GetNumRaidMembers() == 0 then return; end;
-	if not IsRaidOfficer() then return; end;
-	
+	if GetNumRaidMembers() > 0 and not IsRaidOfficer() then return; end
+
 	if event == "READY_CHECK" then
 		self:OnReadyCheck(...);
 	elseif event == "READY_CHECK_CONFIRM" then
@@ -20,17 +19,17 @@ end);
 
 function AssistantReadyCheck:OnReadyCheck(name, time)
 	if name == UnitName("player") then return; end;
-	
+
 	readyCheckPlayers, readyCheckAllReady = { [name] = true }, true;
 end
 
 function AssistantReadyCheck:OnPlayerConfirm(unit, ready)
 	if not readyCheckPlayers then return; end;
-	if unit == "player" or unit:match("^party") then return; end;
-	
+	if unit == "player" then return; end;
+
 	local name = UnitName(unit);
 	readyCheckPlayers[name] = ready;
-	
+
 	if not ready then
 		readyCheckAllReady = false;
 		self:Print(RAID_MEMBER_NOT_READY, name);
@@ -40,9 +39,9 @@ end
 function AssistantReadyCheck:OnReadyCheckFinished(preempted)
 	if not readyCheckPlayers then return; end;
 	if preempted then return; end;			-- ignore Blizzard's bad design
-	
+
 	local numberAFK, afkNames = self:GetAFKPlayers();
-	
+
 	if numberAFK > 0 then
 		self:Print(RAID_MEMBERS_AFK, afkNames);
 	elseif not readyCheckAllReady then
@@ -50,16 +49,16 @@ function AssistantReadyCheck:OnReadyCheckFinished(preempted)
 	else
 		self:Print(READY_CHECK_ALL_READY);
 	end
-	
+
 	readyCheckPlayers, readyCheckAllReady = nil, nil;
 end
 
 function AssistantReadyCheck:GetAFKPlayers()
 	local number, names, spacer = 0, "", "";
-	
+
 	for index = 1, 40 do
 		local name = UnitName("raid" .. index);
-		
+
 		if not name then
 			-- empty raid slot
 		elseif readyCheckPlayers[name] == nil then
@@ -70,7 +69,7 @@ function AssistantReadyCheck:GetAFKPlayers()
 			-- player is ready
 		end
 	end
-	
+
 	return number, names;
 end
 
